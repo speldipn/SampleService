b3dc4c0a2efaa627a3cef1da33f2c43ab3d98493
Oh, Joon young <speldipn@gmail.com>
2018-01-17 07:29:05 +0900

    Initial commit

---
+++ app/src/main/java/com/example/spdn/sampleservice/SmsReceiver.java
@@ -0,0 +1,53 @@
+package com.example.spdn.sampleservice;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.util.Log;
+
+import java.util.Date;
+
+public class SmsReceiver extends BroadcastReceiver {
+  public static final String TAG = "SmsReceiver";
+
+  @Override
+  public void onReceive(Context context, Intent intent) {
+    Log.i(TAG, "OnReceive() 메소드 호출됨.");
+
+    Bundle bundle = intent.getExtras();
+    SmsMessage[] messages = parseSmsMessage(bundle);
+
+    if (messages != null && messages.length > 0) {
+      String sender = messages[0].getOriginatingAddress();
+      Log.i(TAG, "SMS sender : " + sender);
+
+      String contents = messages[0].getMessageBody().toString();
+
+      Log.i(TAG, "SMS contents : " + contents);
+
+      Date receivedData = new Date(messages[0].getTimestampMillis());
+      Log.i(TAG, "SMS received date : " + receivedData.toString());
+    }
+  }
+
+  private SmsMessage[] parseSmsMessage(Bundle bundle) {
+    Object[] objs = (Object[]) bundle.get("pdus");
+    SmsMessage[] messages = new SmsMessage[objs.length];
+
+    int smsCount = objs.length;
+    for (int i = 0; i < smsCount; i++) {
+      // PDU 포맷으로 되어있는 메세지를 복원합니다.
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        String format = bundle.getString("format");
+        messages[i] = SmsMessage.createFromPdu((byte[]) objs[i], format);
+      } else {
+        messages[i] = SmsMessage.createFromPdu((byte[]) objs[i]);
+      }
+    }
+
+    return messages;
+  }
+}
